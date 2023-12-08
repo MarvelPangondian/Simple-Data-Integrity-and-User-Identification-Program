be7e93a54c36cfaabac472f2d6886460a8ef0786
Marvel Pangondian <marvel.pangondian01@gmail.com>
2023-12-08 21:16:31 +0700

    feat: added gitignore and a simple prototype

---
+++ main.py
@@ -0,0 +1,23 @@
+import hashlib
+import json
+import random
+
+
+def generate_random_prime():
+    pass
+
+def generate_coprime(num):
+    pass
+
+def generate_random_key():
+    pass
+
+
+def generate_signature():
+    pass
+
+def load_database():
+    pass
+
+def save_database():
+    pass
